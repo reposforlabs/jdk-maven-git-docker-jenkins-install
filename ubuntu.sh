cf18d95ef5a498d7d048452cb456879cdcbe4078
reposforlabs <160736679+reposforlabs@users.noreply.github.com>
2024-11-18 15:55:16 +0530

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -43,4 +43,4 @@ echo \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
